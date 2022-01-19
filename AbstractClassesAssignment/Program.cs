589e23a2d564682ca76f6c524cc6e47f5f630775
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-19 09:07:18 +0000

    Add files via upload

---
+++ AbstractClassesAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee iName = new Employee
+            {
+                firstName = "Sample",
+                lastName = " Student"
+            };
+
+            iName.SayName();
+            Console.ReadLine();
+
+            Employee leave = new Employee();
+            {
+                leave.Quit();
+                Console.ReadLine();
+            }
+        }
+    }
+}
