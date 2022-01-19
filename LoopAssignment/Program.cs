425c9a09095f443d3cd310f13d37c99c98204d16
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-19 09:29:40 +0000

    Add files via upload

---
+++ LoopAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 20;
+            //while (i <= 30)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+            int i = 9;
+
+            do
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+            } while (i < 60);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAssignment/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Operator
+    {
+        public double GetAdd(int data)
+        {
+            return data + 7;
+        }
+
+        public double GetSubtract(double data)
+        {
+            return data - 4;
+        }
+
+        public double GetDivide(double data)
+        {
+            return data / 2;
+        }
+    }
+}
