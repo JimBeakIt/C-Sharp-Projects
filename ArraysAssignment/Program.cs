5153b30d4cfc5ca1dd579d33e26feb6a35a554c0
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-19 09:09:43 +0000

    Add files via upload

---
+++ ArraysAssignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = { "Hello", "Cake", "Bye", "Car", "Sigh", "Laugh", "Moon" };
+
+            Console.WriteLine("Select an index between 0 and 6, I'll then display the string at that index on the screen.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > 6)
+            {
+                Console.WriteLine("This index doesn't exist.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(strArray[number1]);
+                Console.ReadLine();
+            }
+
+            int[] intArray = { 32, 1, 23, 41, 2, 9, 15 };
+
+            Console.WriteLine("Select an index between 0 and 6, I'll then display the interger at that index on the screen.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number2 > 6)
+            {
+                Console.WriteLine("This index doesn't exist.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(intArray[number2]);
+                Console.ReadLine();
+            }
+
+            List<string> intList = new List<string>();
+            intList.Add("Hi");
+            intList.Add("James");
+            intList.Add("Good");
+            intList.Add("Morning");
+
+            Console.WriteLine("Select an index between 0 and 3, then I'll display the content at that index on the screen.");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            if (number3 > 3)
+            {
+                Console.WriteLine("This index doesn't exist.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(intList[number3]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BooleanLogicAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string whatAge = Console.ReadLine();
+            int whaAge = Convert.ToInt32(whatAge);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI violation? Please enter 'True' or 'False'.");
+            string duiV = Console.ReadLine();
+            bool dV = Convert.ToBoolean(duiV);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedT = Console.ReadLine();
+            int speT = Convert.ToInt32(speedT);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool result = (whaAge > 15 && dV == false && speT < 4);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
