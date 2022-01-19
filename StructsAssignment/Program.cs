1338dd96f96ecb8f1e590b611e0b4e70245ecd86
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-19 09:35:36 +0000

    Add files via upload

---
+++ StructsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1;
+
+            number1.Amount = 21;
+
+            Console.WriteLine(number1.Amount);
+
+            Console.ReadKey();
+        }
+    }
+}
