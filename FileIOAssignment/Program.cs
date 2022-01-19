9b0631ce3d71a96adcecd325fcc104089c35ef12
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-19 09:24:47 +0000

    Add files via upload

---
+++ FileIOAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number I can turn to a text file");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(userNum);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\jimbe\Logs\logNum.txt", true))
+            {
+                file.WriteLine(userNum);
+            }
+        }
+    }
+}
